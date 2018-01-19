b6a87ae8928fcf04d5709217b0a974247cad108a
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2018-01-18 20:29:01 -0500

    new status

---
+++ db.php
@@ -0,0 +1,24 @@
+<?php
+$dbname = 'oberlin_environmentaldashboard';
+$production_server = (posix_uname()['nodename'] === 'environmentaldashboard.org');
+if ($production_server) { // mysql server is on same machine as web server
+  require '../secret/local.php';
+} else { // connect to mysql server remotely
+  require '../secret/remote.php';
+}
+
+try {
+  $db = new PDO($con, "{$username}", "{$password}"); // cast as string bc cant pass as reference
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+}
+catch (PDOException $e) { die($e->getMessage()); }
+
+if (isset($_SERVER['REQUEST_URI']) && $production_server) { // The browser sets REQUEST_URI, so it will not be set for scripts run on command line
+  $stmt = $db->prepare('SELECT id FROM users WHERE slug = ?');
+  $stmt->execute(array(explode('/', $_SERVER['REQUEST_URI'])[1]));
+  $user_id = intval($stmt->fetchColumn());
+} else {
+  $user_id = 1; // Default to Oberlin
+}
+?>
